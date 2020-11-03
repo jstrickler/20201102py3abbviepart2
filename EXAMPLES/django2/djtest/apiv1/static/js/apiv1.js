ce51dfa8983dfc6c5194b162f44d72dd320bf752
jstrickler <jstrickler@gmail.com>
2020-11-03 10:17:43 -0500

    updated PDF

--- EXAMPLES/django2/djtest/apiv1/static/js/apiv1.js
+++
@@ -1,8 +0,0 @@
-// your javascript goes here
-$(document).ready(function() {
-  $.get("/api/v1/hero/1").success(function (data) {
-      $("#hero").html("<h1>apiv1.js</h1>");
-      console.log(data);
-    }
-  );
-});
